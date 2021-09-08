bb5baab11eb7d1a84676287c4cfe98f706fdbccc
Marco Colia <info@justoverclock.it>
2021-09-08 09:37:32 +0200

    fixes to code and compiled for production

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,23 +5,16 @@ import DiscussionListItem from 'flarum/common/components/DiscussionListItem';
 app.initializers.add('justoverclock/discussion-info', () => {
     extend(DiscussionListItem.prototype, 'infoItems', function (items) {
         const discussion = this.attrs.discussion;
-        console.log(discussion.data.attributes);
         items.add(
             'replyCount',
             m('div', { className: 'replyCountBox' }, [
                 m('i', { className: 'fas fa-users icontext' }),
-                m('strong', app.translator.trans('justoverclock-discussion-info.forum.participantCount') + ': '),
-                    discussion.data.attributes.participantCount,
+                  m('strong', app.translator.trans('justoverclock-discussion-info.forum.participantCount') + ': '),
+                discussion.data.attributes.participantCount,
+                m('i', { className: 'fas fa-clock icontext' }),
+                  m('strong', app.translator.trans('justoverclock-discussion-info.forum.createdAt') + ': '),
+                discussion.data.attributes.createdAt.slice(0, 10),
             ])
         );
-        items.add(
-          'lastReadAt',
-          m('div', { className: 'lastReadAt' }, [
-            m('i', { className: 'fas fa-clock icontext' }),
-            m('strong', app.translator.trans('justoverclock-discussion-info.forum.lastReadAt') + ': '),
-            discussion.data.attributes.lastReadAt,
-          ])
-        )
     });
 });
-
